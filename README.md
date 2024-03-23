3b46a66fb84e9ec0074135a68f80248ba8ff27a9
Kilian PAQUIER <kilian@kilianpaquier.com>
2024-03-23 04:29:59 +0100

    refactor(tests): better print and handling of compare diff with go-diff

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
+<!-- This file is safe to edit. Once it exists it will not be overwritten. -->
+
 # filesystem <!-- omit in toc -->
+
+<p align="center">
+  <img alt="GitHub Actions" src="https://img.shields.io/github/actions/workflow/status/kilianpaquier/filesystem/integration.yml?branch=main&style=for-the-badge">
+  <img alt="GitHub Release" src="https://img.shields.io/github/v/release/kilianpaquier/filesystem?include_prereleases&sort=semver&style=for-the-badge">
+  <img alt="GitHub Issues" src="https://img.shields.io/github/issues-raw/kilianpaquier/filesystem?style=for-the-badge">
+  <img alt="GitHub License" src="https://img.shields.io/github/license/kilianpaquier/filesystem?style=for-the-badge">
+  <img alt="Coverage" src="https://img.shields.io/codecov/c/github/kilianpaquier/filesystem/main?style=for-the-badge">
+  <img alt="Go Version" src="https://img.shields.io/github/go-mod/go-version/kilianpaquier/filesystem/main?style=for-the-badge&label=Go+Version">
+  <img alt="Go Report Card" src="https://goreportcard.com/badge/github.com/kilianpaquier/filesystem?style=for-the-badge">
+</p>
+
+---
 
 - [How to use ?](#how-to-use-)
 - [Features](#features)
--- pkg/tests/compare_dir.go
+++ pkg/tests/compare_dir.go
@@ -7,7 +7,6 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -51,28 +50,10 @@ func readDirInMap(srcdir string) (map[string]string, error) {
 	return files, errors.Join(errs...)
 }
 
-// EqualDirOpt represents an option to be given to AssertEqualDir to tune its behavior.
-type EqualDirOpt func(e *equalDirClient)
-
-// IgnoreFunc represents a function to be given with WithIgnoreDiff to ignore a specific difference in AssertEqualDir.
-type IgnoreFunc func(filename string, item diffmatchpatch.Diff) bool
-
-// equalDirClient represents the client option of AssertEqualDir.
-type equalDirClient struct {
-	ignore []IgnoreFunc
-}
-
-// WithIgnoreDiff is an option to give to AssertEqualDir to ignore specific differences during execution.
-func WithIgnoreDiff(ignore IgnoreFunc) EqualDirOpt {
-	return func(e *equalDirClient) {
-		e.ignore = append(e.ignore, ignore)
-	}
-}
-
 // AssertEqualDir compares expected an actual directories (and their subdirectories).
 //
 // It will fail with t in case a file is missing in actual, a file is present in actual but not in expected and if the content of any file in actual is not the same as its peer in expected.
-func AssertEqualDir(t testing.TB, expected, actual string, opts ...EqualDirOpt) {
+func AssertEqualDir(t testing.TB, expected, actual string) {
 	// read all files in expected directory
 	expectedFiles, err := readDirInMap(expected)
 	assert.NoError(t, err, "failed to completely read expected %s folder and its children", expected)
@@ -80,13 +61,6 @@ func AssertEqualDir(t testing.TB, expected, actual string, opts ...EqualDirOpt) 
 	// read all files in actual directory
 	actualFiles, err := readDirInMap(actual)
 	assert.NoError(t, err, "failed to completely read actual %s folder and its children", actual)
-
-	client := &equalDirClient{}
-	for _, opt := range opts {
-		if opt != nil {
-			opt(client)
-		}
-	}
 
 	// check all expected contents against actual contents
 	for expectedFilename, expectedContent := range expectedFiles {
@@ -96,43 +70,15 @@ func AssertEqualDir(t testing.TB, expected, actual string, opts ...EqualDirOpt) 
 			continue
 		}
 
-		matcher := diffmatchpatch.New()
-		diffs := matcher.DiffMain(expectedContent, actualContent, false)
-
-		// filter retrieved diffs
-		diffs = client.filterDiffs(expectedFilename, diffs)
-		assert.Len(t, diffs, 0, "%s is different between expected and actual folder: %s", expectedFilename, matcher.DiffPrettyText(diffs))
+		diffs, ok := Diffs(expectedContent, actualContent)
+		if !ok {
+			patch := DiffsToPatch(diffs)
+			assert.Fail(t, expectedFilename+" is different from expected", patch)
+		}
 	}
 
 	for actualFilename := range actualFiles {
 		_, ok := expectedFiles[actualFilename]
 		assert.True(t, ok, "%s is present in actual directory but not in expected one", actualFilename)
 	}
-}
-
-// filterDiffs filters input diffs with the equalDirClient options.
-func (e equalDirClient) filterDiffs(filename string, diffs []diffmatchpatch.Diff) []diffmatchpatch.Diff {
-	filtered := make([]diffmatchpatch.Diff, 0, len(diffs))
-	for _, diff := range diffs {
-		// ignore diff equals
-		// ignore windows / linux diffs
-		if diff.Type == diffmatchpatch.DiffEqual || diff.Text == "\r" {
-			continue
-		}
-
-		// ignore some diffs according to input options
-		isIgnored := false
-		for _, ignore := range e.ignore {
-			if ignore(filename, diff) {
-				isIgnored = true
-				break
-			}
-		}
-
-		// add diff to filtered diffs in case it has to be kept
-		if !isIgnored {
-			filtered = append(filtered, diff)
-		}
-	}
-	return filtered
 }
